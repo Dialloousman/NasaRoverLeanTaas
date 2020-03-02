9b9d4bdd79e0891017946d3d1ebcf11d987a9f52
Ousman <ordiallo@gmail.com>
2020-03-02 18:27:49 -0500

    placeholder for cameras sol component containter set

--- public/App.jsx
+++ public/App.jsx
@@ -1,15 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import camerasSolContainer from './components/camerasSolContainer.jsx';
+import React, { useState, useEffect } from 'react';
+import CamerasSolContainer from './components/camerasSolContainer.jsx';
 
- const App = () => {
-    return (
-        <div>
-            <camerasSolContainer/>
-            <h1>
-                Hello World Baby!
-            </h1>
-        </div>
-    )
-};
+const App = () => (
+  <div>
+    <CamerasSolContainer />
+    <h1>
+      Hello World Baby!
+    </h1>
+  </div>
+);
 
 export default App;
--- public/components/camerasSolContainer.jsx
+++ public/components/camerasSolContainer.jsx
@@ -1,11 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
- const camerasSolContainer = () => {
-    return (
-        <div>
-            Hello from cameras sol container
-        </div>
-    )
-};
+const CamerasSolContainer = () => (
+  <div>
+    Hello from cameras sol container
+  </div>
+);
 
-export default camerasSolContainer;
+export default CamerasSolContainer;
