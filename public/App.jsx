3e6b460aac856753cef51dc5cd18cb17f27bd4ba
Ousman <ordiallo@gmail.com>
2020-03-04 10:53:00 -0500

    buttons added to CamerasSolContainer component

--- public/App.jsx
+++ public/App.jsx
@@ -3,13 +3,35 @@ import CamerasSolContainer from './components/CamerasSolContainer.jsx';
 import CameraInfoCard from './components/CameraInfoCard.jsx';
 
 
-const App = () => (
-  <div data-test="mainAppTest">
-    <CamerasSolContainer />
-    <main>
-      <CameraInfoCard />
-    </main>
-  </div>
-);
+const App = () => {
+  const [dataFetchedFromNasaApi, updateDataFetchedFromNasaApi] = useState([]);
+
+  const fetchFromNasaRoverApi = () => {
+    const apiURI = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY';
+
+    fetch(apiURI)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data from NASA', data);
+        updateDataFetchedFromNasaApi(data);
+      });
+  };
+
+//   useEffect(() => {
+//     // Update the document title using the browser API
+//     console.log('--->APP HAS MOUNTED** FROM USE EFFECT');
+//     fetchFromNasaRoverApi();
+//     console.log('CURRENT DATA STATE', dataFetchedFromNasaApi);
+//   }, [dataFetchedFromNasaApi[0]]);
+
+  return (
+    <div data-test="mainAppTest">
+      <CamerasSolContainer />
+      <main>
+        <CameraInfoCard />
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- public/components/camerasSolContainer.jsx
+++ public/components/camerasSolContainer.jsx
@@ -2,8 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 const CamerasSolContainer = () => (
   <div id="camerasSolContainer" data-test="CameraSolComponent">
-    <li>Cameras</li>
-    <li>Sol</li>
+    <div>
+      <li>Cameras</li>
+      <button>7 Selected</button>
+    </div>
+    <div>
+      <li>Sol</li>
+      <button>14</button>
+    </div>
   </div>
 );
 
